263cb8598fc2950881274aab9b883f6cd410ae0c
Anthony Villegas <villegasanthony87@yahoo.com>
2024-08-02 11:02:09 -0500

    feat: scoped slots added

--- cmp_adv/adv_cmp/src/components/BaseCard.vue
+++ cmp_adv/adv_cmp/src/components/BaseCard.vue
@@ -1,14 +1,27 @@
 <template>
   <div>
+    <header v-if="$slots.header">
+      <slot name="header">
+        <!-- <h2>The Default</h2> -->
+      </slot>
+    </header>
     <slot></slot>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  mounted() {},
+};
 </script>
 
 <style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
 div {
   margin: 2rem auto;
   max-width: 30rem;
