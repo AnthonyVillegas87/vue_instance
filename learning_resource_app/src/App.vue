08fe0403c9962b965fc3ae76eeeaea7f78b9919d
Anthony Villegas <villegasanthony87@yahoo.com>
2024-08-08 14:30:11 -0500

    feat: created components for learning resources.

--- learning_resource_app/src/App.vue
+++ learning_resource_app/src/App.vue
@@ -1,25 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import StoredResources from "./components/learning-resources/StoredResources.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { StoredResources },
+
+  data() {
+    return {
+      storedResources: [
+        {
+          id: "official-guide",
+          title: "Official Guide",
+          description: "The official Vue.js documentation.",
+          link: "https://vuejs.org",
+        },
+        {
+          id: "design-gurus",
+          title: "Design Gurus",
+          description: "Learn System Design",
+          link: "https://designgurus.io",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }
