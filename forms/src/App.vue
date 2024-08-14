369775ae8cccbd8e2cf3117cbb9305644ea6a617
Anthony Villegas <villegasanthony87@yahoo.com>
2024-08-14 08:38:48 -0500

    feat: Form component w/ styling created

--- forms/src/App.vue
+++ forms/src/App.vue
@@ -1,14 +1,32 @@
-<template></template>
+<template>
+  <the-form></the-form>
+</template>
 
-<script></script>
+<script>
+import TheForm from "./components/TheForm.vue";
+export default {
+  components: {
+    TheForm,
+  },
+};
+</script>
 
 <style>
-#app {
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  background-color: #292929;
+}
+
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
