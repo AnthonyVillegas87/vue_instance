7f2e28ef65b8798ce598d08e48b90cda0cefc16f
Anthony Villegas <villegasanthony87@yahoo.com>
2024-08-16 09:35:07 -0500

    proj: http_request proj created

---
+++ http_req/src/App.vue
@@ -0,0 +1,43 @@
+template>
+  <learning-survey @survey-submit="storeSurvey"></learning-survey>
+  <user-experiences :results="savedSurveyResults"></user-experiences>
+</template>
+
+<script>
+import LearningSurvey from './components/survey/LearningSurvey.vue';
+import UserExperiences from './components/survey/UserExperiences.vue';
+
+export default {
+  components: {
+    LearningSurvey,
+    UserExperiences,
+  },
+  data() {
+    return {
+      savedSurveyResults: [],
+    };
+  },
+  methods: {
+    storeSurvey(surveyData) {
+      const surveyResult = {
+        name: surveyData.userName,
+        rating: surveyData.rating,
+        id: new Date().toISOString(),
+      };
+      this.savedSurveyResults.push(surveyResult);
+      console.log(surveyResult);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
