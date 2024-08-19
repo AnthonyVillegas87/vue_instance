45edd1a72ab2f46e4f336dbb42bfd3058b3a57e2
Anthony Villegas <villegasanthony87@yahoo.com>
2024-08-19 10:41:39 -0500

    proj: setup for building multi-page SPA's

--- http_req/src/components/survey/LearningSurvey.vue
+++ http_req/src/components/survey/LearningSurvey.vue
@@ -41,6 +41,7 @@
         <p v-if="invalidInput">
           One or more input fields are invalid. Please check your provided data.
         </p>
+        <p v-if="error">{{ error }}</p>
         <div>
           <base-button>Submit</base-button>
         </div>
@@ -56,6 +57,7 @@ export default {
       enteredName: "",
       chosenRating: null,
       invalidInput: false,
+      error: null,
     };
   },
   // emits: ["survey-submit"],
@@ -71,7 +73,7 @@ export default {
       //   userName: this.enteredName,
       //   rating: this.chosenRating,
       // });
-
+      this.error = null;
       fetch("https://vue-http-fb186-default-rtdb.firebaseio.com/serveys.json", {
         method: "POST",
         headers: {
@@ -81,7 +83,17 @@ export default {
           name: this.enteredName,
           rating: this.chosenRating,
         }),
-      });
+      })
+        .then((response) => {
+          if (response.ok) {
+            // ....
+          } else {
+            throw new Error("Failed to submit the survey.");
+          }
+        })
+        .catch((error) => {
+          this.error = error.message;
+        });
 
       this.enteredName = "";
       this.chosenRating = null;
