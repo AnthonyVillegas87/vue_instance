54ccee8a842f2c7349ba74f8bf4bc74e3668500c
Anthony Villegas <villegasanthony87@yahoo.com>
2024-08-19 15:46:55 -0500

    feat: Utilized nested route in TeamsList component.

--- routing/src/main.js
+++ routing/src/main.js
@@ -5,13 +5,18 @@ import { createRouter, createWebHistory } from "vue-router";
 import TeamsList from "./components/teams/TeamsList.vue";
 import UsersList from "./components/users/UsersList.vue";
 import TeamMembers from "./components/teams/TeamMembers.vue";
-
+import NotFound from "./components/nav/NotFound.vue";
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: "/teams", component: TeamsList },
+    { path: "/", redirect: "/teams" },
+    {
+      path: "/teams",
+      component: TeamsList,
+      children: [{ path: ":teamId", component: TeamMembers, props: true }],
+    },
     { path: "/users", component: UsersList },
-    { path: "/teams/:teamId", component: TeamMembers },
+    { path: "/:notFound(.*)", component: NotFound },
   ],
   linkActiveClass: "active",
 });
